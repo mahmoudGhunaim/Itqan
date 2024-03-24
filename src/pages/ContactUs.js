c955530b6a8fd4e82745cb9706281202bc07c530
mahmoudGhunaim <141932536+mahmoudGhunaim@users.noreply.github.com>
2024-03-24 13:32:09 +0300

    start responseve

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -8,9 +8,13 @@ const ContactUs = () => {
     <Layout>
         <Hero title="الاتصال بنا" subTitle="“التواصل”"/>
         <ScrollToTopButton/>
+        <div className='Contact-footer-section-wallpaper'>
           <section className='Contact-footer-section'>
+       
+               
         <div className='Contact-footer-container'>
-            <form className='Contact-footer-form' >
+           <form className='Contact-footer-form' >
+                
                 <label>
                 الاسم       
                     <input
@@ -38,14 +42,32 @@ const ContactUs = () => {
                 <button type='submit'>إرسال</button>
                 <p>نحن ملتزمون بحماية واحترام خصوصيتكم. سيتم استخدام المعلومات المقدمة في هذا النموذج للتواصل معكم والإجابة على استفساراتكم فقط.</p>
             </form>
+            
             <div className='Contact-footer-content'>
                 <h2>اتصل بنا</h2>
                 <p>نحن هنا للإجابة على استفساراتكم وتقديم الدعم. يرجى ملء النموذج وسيقوم أحد ممثلينا بالتواصل معكم في أقرب وقت ممكن.</p>
                 <h3>معلومات الاتصال</h3>
+                <div className='Contact-info'>
+                 <div className='Contact-info-data'>
                 <h6>البريد الإلكتروني</h6>
                 <p>info@itqancapital.com</p>
-                <h6>العنوان</h6>
-                <p>المملكة العربية السعودية / الرياض......</p>
+                </div>
+                <div className='Contact-info-data'>
+                    <h6>رقم الهاتف</h6>
+                    <p>Jeddah: +966 12 263 8787 – Ext. (615) <br/>Riyadh: 8001240533 +966 11 2164333</p>
+                </div>
+                </div>
+                <div className='Contact-info'>
+                    <div className='Contact-info-data'>
+                      <h6> 1 العنوان</h6>
+                         <p>المملكة العربية السعودية / الرياض......</p>
+                    </div>
+                    <div className='Contact-info-data'>
+                      <h6> 2 العنوان</h6>
+                         <p>المملكة العربية السعودية / الرياض......</p>
+                    </div>
+                </div>
+                
                 <h6>التواصل الاجتماعي</h6>
                 <div className='Contact-footer-content-social'> 
                     <img src='/X.png'/>
@@ -53,7 +75,9 @@ const ContactUs = () => {
                 </div>
             </div>
         </div>
+        
     </section>
+    </div>
     </Layout>
     )
 }
