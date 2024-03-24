787d974658a4e924eb8f4a476262ca1578ea9790
mahmoudGhunaim <141932536+mahmoudGhunaim@users.noreply.github.com>
2024-03-24 14:11:42 +0300

    hambu test

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,13 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import "./style/layout.css";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 const Header = ({ siteTitle }) => {
-  const [showDropdown, setShowDropdown] = React.useState();
+  const [showDropdown, setShowDropdown] = React.useState(false);
+  const [modal, setModal] = React.useState(false);
+
+  const toggle = () => setModal(!modal);
 
   return (
     <header className="navBar-sec">
@@ -12,14 +16,14 @@ const Header = ({ siteTitle }) => {
           <img src="/itqanlogo.svg" alt="Logo"/>
         </div>
         <div className="navBar-links">
-          <Link to={`../../../`}>الرئيسية</Link>
+          <Link to="/">الرئيسية</Link>
           <Link to="/AboutUS">نبذة عنا</Link>
           <div className="dropdown">
-            <Link to="/AboutUS" onMouseEnter={() => setShowDropdown(true)} onMouseLeave={() => setShowDropdown()}>
+            <Link to="/Services" onMouseEnter={() => setShowDropdown(true)} onMouseLeave={() => setShowDropdown(false)}>
               الخدمات
             </Link>
             {showDropdown && (
-              <div className="dropdown-content" onMouseEnter={() => setShowDropdown(true)} onMouseLeave={() => setShowDropdown()}>
+              <div className="dropdown-content" onMouseEnter={() => setShowDropdown(true)} onMouseLeave={() => setShowDropdown(false)}>
                 <Link to="/AssetManagement">إدارة الأصول</Link>
                 <Link to="/ConservationServices">خدمات الحفظ</Link>
                 <Link to="/InvestmentBanking">الخدمات المصرفية الاستثمارية</Link>
@@ -27,14 +31,45 @@ const Header = ({ siteTitle }) => {
               </div>
             )}
           </div>
-          <Link to="/AboutUS">خدمة العملاء</Link>
-          <Link to="/AboutUS">التصريحات</Link>
-          <Link to="/AboutUS">المصادر</Link>
+          <Link to="/CustomerService">خدمة العملاء</Link>
+          <Link to="/Statements">التصريحات</Link>
+          <Link to="/Resources">المصادر</Link>
           <Link to="/ContactUs">الاتصال بنا</Link>
         </div>
         <div className="navBar-openAcc">
-          <Link to="/AboutUs"><button>فتح حساب جديد</button></Link>
+          <Link to="/OpenAccount"><button>فتح حساب جديد</button></Link>
         </div>
+        <div className='mobile-menu'>
+          <Button color="danger" onClick={toggle}>
+            <img src='/menu.png' alt="Menu"/>
+          </Button>
+          <Modal isOpen={modal} toggle={toggle}>
+            <ModalHeader toggle={toggle}></ModalHeader>
+            <ModalBody>
+              <div className="logo-sec-mob">
+                <Link to="/"><img src="/Restnest.svg" alt="Restnest Logo"/></Link>
+              </div>
+              <div className='navBar-hamburger'>
+              <Link to="/">الرئيسية</Link>
+          <Link to="/AboutUS">نبذة عنا</Link>
+          <Link to="/AssetManagement">إدارة الأصول</Link>
+                <Link to="/ConservationServices">خدمات الحفظ</Link>
+                <Link to="/InvestmentBanking">الخدمات المصرفية الاستثمارية</Link>
+                <Link to="/AdvisoryResearch">البحوث والمشورة</Link>
+              <Link to="/CustomerService">خدمة العملاء</Link>
+          <Link to="/Statements">التصريحات</Link>
+          <Link to="/Resources">المصادر</Link>
+          <Link to="/ContactUs">الاتصال بنا</Link>
+         
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <div className="login-mob">
+                <Link to='https://dashboard.hostaway.com/login' className='login-btn'>Owner Login</Link>
+              </div>
+              </ModalFooter>
+            </Modal>
+    </div>
       </div>
     </header>
   );
