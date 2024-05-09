d0b4e1f40b5bf85b3ac0c6ad2b93992336c53e80
mahmoudGhunaim <141932536+mahmoudGhunaim@users.noreply.github.com>
2024-05-09 16:45:17 +0300

    some fixed itqan 52

--- src/pages/companies-login.js
+++ src/pages/companies-login.js
@@ -249,7 +249,7 @@ const CompaniesLogin = () => {
             const response = await axios.post("https://itqan-strapi.softylus.com/api/companies-logins", { data: formData }, {
                 headers: {
                     'Content-Type': 'application/json',              
-                    "Authorization": "Bearer 662516a59e6dbdcaf0e28198cbaaa8f40c29f4076ba645e5d521ccbc234370f13da8e9fab43d30a47cbfd96461483c6f97d4b48859958b1399addc63ac80cc139b62b3f3799835893df308b2c131329a97f1ad993ead75036119f2fa767c0d35a1545cb77a6b5edf9fb2639df381d7d630891fad18b0770be9a2db0c2cb3e9a7"
+                    "Authorization": "Bearer 848485480979d1216343c88d697bd91d7e9d71cacffad3b1036c75e10813cc5849955b2fb50ea435089aa66e69976f378d4d040bc32930525651db4ad255615c24947494ddef876ec208ef49db6ba43f4a2eb05ddbee034e2b01f54741f2e9ea2f1930a4181d602dc086b7cde8a871f48d63596e07356bf2a56749c7c4f20b6c"
                 }
             });
             if (response.status === 200) {
