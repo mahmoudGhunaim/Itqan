73f9da8b7b147dd4d35f68f49a747969c1958c79
MahmoudGhunaim <a93153296@gmail.com>
2024-07-11 18:02:45 +0300

    Some modifications 7 from Mahmoud

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { FormattedMessage } from 'react-intl';
-import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import ItqanC from '../components/ItqanC';
 import Distinction from '../components/distinction';
@@ -9,31 +9,55 @@ import '../components/style/AboutUs.css';
 import ScrollToTopButton from '../components/ScrollToTopButton';
 import Hero from '../components/Hero';
 import Seo from '../components/seo';
+import { useLocalization } from '../context/LocalizationContext';
 
 const AboutUS = () => {
+  const [pageData, setPageData] = useState(null);
+  const { locale } = useLocalization();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://itqan-strapi.softylus.com/api/pages/?filters[custom_slug][$eq]=about_us&populate[sections][populate][section_content][populate][single_card][populate]=*&locale=${locale}&populate=image`, {
+          headers: {
+            Authorization: 'Bearer 848485480979d1216343c88d697bd91d7e9d71cacffad3b1036c75e10813cc5849955b2fb50ea435089aa66e69976f378d4d040bc32930525651db4ad255615c24947494ddef876ec208ef49db6ba43f4a2eb05ddbee034e2b01f54741f2e9ea2f1930a4181d602dc086b7cde8a871f48d63596e07356bf2a56749c7c4f20b6c'
+          }
+        });
+        setPageData(response.data.data[0].attributes);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!pageData) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Layout>
       <Seo
-         title="إتقان كابيتال: الريادة في الاستثمار الإسلامي والخدمات المالية بالمملكة      "
-        description="اكتشف إتقان كابيتال، الشركة السعودية الرائدة في تقديم منتجات وخدمات استثمارية متوافقة مع الشريعة الإسلامية. مع تركيز على الدقة، الشغف، والكمال، نحن ملتزمون بتحقيق أعلى معايير الاستثمار لعملائنا في السوق المالية السعودية وأسواق دول مجلس التعاون الخليجي.      "
+        title={pageData.meta_title}
+        description={pageData.meta_description}
       />
       <ScrollToTopButton />
       <Hero
-        title={<FormattedMessage id="hero_title_about" />}
-        subTitle={<FormattedMessage id="hero_subtitle" />}
+        title={pageData.sections.data[0]?.attributes.section_content[0].title}
+        subTitle={pageData.sections.data[0]?.attributes.section_content[0].subtitle}
       />
       <section className="who-are-we-sec">
         <div className="who-are-we-container">
           <div className="who-are-we-title">
-            <h3><FormattedMessage id="who_we_are" /></h3>
+            <h3>{pageData.sections.data[1]?.attributes.section_content[0].title}</h3>
             <p>
-              <FormattedMessage id="about_us_paragraph_1" />
-            </p>
-            <p>
-              <FormattedMessage id="about_us_paragraph_2" />
-            </p>
-            <p>
-              <FormattedMessage id="about_us_paragraph_3" />
+              {pageData.sections.data[1]?.attributes.section_content[0].subtitle.split('\n').map((text, index) => (
+                <React.Fragment key={index}>
+                  {text}
+                  <br />
+                </React.Fragment>
+              ))}
             </p>
           </div>
           <ItqanC />
